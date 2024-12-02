8b488625dbaa827c9b13da5376a9c671a74bc851
NathanielS9872 <nathanielspr@gmail.com>
2024-12-02 14:15:16 -0600

    Items (Ben) + Events  and Admin Page (Nathaniel)

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -1,7 +1,24 @@
-(async()=>{
+;(async()=>{
+    if (document.title=="Food Truck"){
     const result = await fetch('/api/v1/events')
+    const result1 = await fetch ('/api/v1/menu')
     const events = await result.json()
-    const eventList = document.querySelector(".menu-list")
+    const menus = await result1.json()
+    const eventList = document.querySelector(".event-list")
+    const menuList = document.querySelector(".menu-list")
+ 
+    menus.forEach(({name, description, price, image}) => {
+        const div = document.createElement("div")
+        div.className = "menu"
+        div.innerHTML = `
+        <img src="${image}" alt="${name}", width="150", height="150">
+        <h2>${name}</h2>
+        <p>${description}</p>
+        <p><strong>Price:</strong> ${price}</p>
+        `
+    menuList.appendChild(div)
+    })
+ 
     events.forEach(({name, location, date, time}) => {
         const div = document.createElement("div")
         div.className = "event"
@@ -11,4 +28,5 @@
         `
     eventList.appendChild(div)
     }
-)})()
+ 
+)}})()
